e07e81300843aab90d08b89e63bfad670965a398
mhasan0505 <mhasan.coder@gmail.com>
2024-09-29 01:19:19 +0600

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import { assets } from '../assets/assets'
 function Navbar() {
   return (
@@ -22,7 +22,24 @@ function Navbar() {
             <hr  className='w-2/4 border-none h-[3.5px] bg-red-700 hidden'/>
           </NavLink>
         </ul>
-        
+        <div className='flex items-center gap-6'>
+          <img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+          <div className='group relative'>
+            <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
+            <div className='group-hover:block absolute hidden dropdown-menu right-0 pt-4'>
+              <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded   '>
+                <p className='cursor-pointer hover:text-black'>My Profile</p>
+                <p className='cursor-pointer hover:text-black'>Orders</p>
+                <p className='cursor-pointer hover:text-black'>Logout</p>
+              </div>
+            </div>
+          </div>
+          <Link to={"/cart"} className='relative'>
+          <img className='w-5 min-w-5' src={assets.cart_icon} alt="" />
+          <p className='absolute bottom-[-5px] right-[-5px] text-center  bg-red-700 text-white w-4 rounded-full loading-4 aspect-square text-[12px]'>0</p>
+          </Link>
+          <img className='w-5 cursor-pointer sm:hidden' src={assets.menu_icon} alt="" />
+        </div>
         
     </div>
   )
